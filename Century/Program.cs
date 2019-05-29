ac63021874b7c86c089334f477883b422444d463
milomacphail <milomacphail@gmail.com>
2019-05-29 18:49:29 -0400

    refactored to ternary statement

---
+++ Century/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Century
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            centuryFromYear(1982)
+        }
+    }
+}
---
+++ Century/centuryFromYear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Century
+{
+    public class centuryFromYear
+    {
+        public centuryFromYear()
+        {
+            int centuryFromYear(int year)
+            {
+                return (int)(year / 100) + ((year % 100 == 0) ? 0 : 1);
+
+            }
+        }
+    }
+    }
+}
